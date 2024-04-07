025dda42f5b43dfb4b0fd2165964f7b2485f36d7
Makeilokoogehena <studworkse@gmail.com>
2024-04-07 20:35:47 +0300

    FEAT: create logic for variables js and four sign

--- js/calc.js
+++ js/calc.js
@@ -17,3 +17,53 @@ function ClearAll() {
 
 const deleteButton = document.querySelector(".calc_delete");
 deleteButton.addEventListener("click", ClearAll);
+
+//
+document.querySelector(".calc__panel").onclick = (e) => {
+  if (!e.target.classList.contains("calc__btn")) return;
+  if (e.target.classList.contains("calc_delete")) return;
+  out.textContent = "";
+  const key = e.target.textContent;
+  if (digit.includes(key)) {
+    if (b === "" && operation === "") {
+      a += key;
+      console.log(a, b, operation);
+      out.textContent = a;
+    } else if (a !== "" && b !== "" && result) {
+    }
+    //
+    else {
+      b += key;
+      out.textContent = b;
+    }
+    console.log(a, b, operation);
+    return;
+  }
+
+  if (action.includes(key)) {
+    operation = key;
+    out.textContent = operation;
+    console.log(a, b, operation);
+    return;
+  }
+
+  if (key === "=") {
+    switch (operation) {
+      case "+":
+        a = +a + +b;
+        break;
+      case "-":
+        a = a - b;
+        break;
+      case "x":
+        a = a * b;
+        break;
+      case "/":
+        a = a / b;
+        break;
+    }
+    result = true;
+    out.textContent = a;
+    console.log(a, b, operation);
+  }
+};
